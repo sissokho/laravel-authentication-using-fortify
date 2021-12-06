4fa4e425cc9a8d3a50fcfcfcb79bd7e581f84cb6
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-12-06 16:20:07 +0000

    Fix wrong condition

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body class="bg-gray-100">
     <main class="flex flex-col justify-start items-center pt-10 min-h-screen">
-        @if (!(session('status') == 'verification-link-sent'))
+        @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
                 A new email verification link has been emailed to you!
             </div>
